42fc3a4108d3acafff54dd8c6af267bd1f24597e
0xNomind <nomind.crypto@gmail.com>
2025-04-10 15:53:30 +0700

    add succintactoe sp1

--- script/src/bin/main.rs
+++ script/src/bin/main.rs
@@ -1,92 +1,72 @@
-//! An end-to-end example of using the SP1 SDK to generate a proof of a program that can be executed
-//! or have a core proof generated.
-//!
-//! You can run this script using the following command:
-//! ```shell
-//! RUST_LOG=info cargo run --release -- --execute
-//! ```
-//! or
-//! ```shell
-//! RUST_LOG=info cargo run --release -- --prove
-//! ```
-
-use alloy_sol_types::SolType;
-use clap::Parser;
-use fibonacci_lib::PublicValuesStruct;
 use sp1_sdk::{include_elf, ProverClient, SP1Stdin};
+use succinctactoe_lib::{play_tic_tac_toe, GameData};
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
-pub const FIBONACCI_ELF: &[u8] = include_elf!("fibonacci-program");
-
-/// The arguments for the command.
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    #[arg(long)]
-    execute: bool,
-
-    #[arg(long)]
-    prove: bool,
-
-    #[arg(long, default_value = "20")]
-    n: u32,
-}
+pub const SUCCINCTACTOE_ELF: &[u8] = include_elf!("succinctactoe-program");
 
 fn main() {
     // Setup the logger.
     sp1_sdk::utils::setup_logger();
-    dotenv::dotenv().ok();
 
-    // Parse the command line arguments.
-    let args = Args::parse();
+    let game_data: GameData = play_tic_tac_toe();
 
-    if args.execute == args.prove {
-        eprintln!("Error: You must specify either --execute or --prove");
-        std::process::exit(1);
-    }
+    println!("Succintactoe SP1 Proof Generator");
+    println!("-------------------------------");
+    println!(
+        "Game data: Username={}, Player Score={}, Computer Score={}",
+        game_data.username, game_data.player_score, game_data.computer_score
+    );
 
     // Setup the prover client.
     let client = ProverClient::from_env();
 
     // Setup the inputs.
     let mut stdin = SP1Stdin::new();
-    stdin.write(&args.n);
+    stdin.write(&game_data.username);
+    stdin.write(&game_data.player_score);
+    stdin.write(&game_data.computer_score);
 
-    println!("n: {}", args.n);
+    // Execute the program
+    let (mut public_values, report) = client.execute(SUCCINCTACTOE_ELF, &stdin).run().unwrap();
 
-    if args.execute {
-        // Execute the program
-        let (output, report) = client.execute(FIBONACCI_ELF, &stdin).run().unwrap();
-        println!("Program executed successfully.");
+    // Record the number of cycles executed.
+    println!(
+        "Program executed successfully with number of cycles: {}",
+        report.total_instruction_count()
+    );
 
-        // Read the output.
-        let decoded = PublicValuesStruct::abi_decode(output.as_slice(), true).unwrap();
-        let PublicValuesStruct { n, a, b } = decoded;
-        println!("n: {}", n);
-        println!("a: {}", a);
-        println!("b: {}", b);
+    // Read values
+    let stored_username = public_values.read::<String>();
+    let stored_user_score = public_values.read::<u32>();
+    let stored_computer_score = public_values.read::<u32>();
+    let result = public_values.read::<bool>();
 
-        let (expected_a, expected_b) = fibonacci_lib::fibonacci(n);
-        assert_eq!(a, expected_a);
-        assert_eq!(b, expected_b);
-        println!("Values are correct!");
+    // Print results
+    println!("Execution results:");
+    println!("- Username: {}", stored_username);
+    println!("- Player Score: {}", stored_user_score);
+    println!("- Computer Score: {}", stored_computer_score);
+    println!("- Result: {}", result);
 
-        // Record the number of cycles executed.
-        println!("Number of cycles: {}", report.total_instruction_count());
-    } else {
-        // Setup the program for proving.
-        let (pk, vk) = client.setup(FIBONACCI_ELF);
+    // Setup the program for proving.
+    let (pk, vk) = client.setup(SUCCINCTACTOE_ELF);
 
-        // Generate the proof
-        let proof = client
-            .prove(&pk, &stdin)
-            .run()
-            .expect("failed to generate proof");
+    // Generate the proof
+    let proof = client
+        .prove(&pk, &stdin)
+        .run()
+        .expect("failed to generate proof");
 
-        println!("Successfully generated proof!");
+    println!("Successfully generated proof!");
 
-        // Verify the proof.
-        client.verify(&proof, &vk).expect("failed to verify proof");
-        println!("Successfully verified proof!");
-    }
+    // Verify the proof.
+    client.verify(&proof, &vk).expect("failed to verify proof");
+    println!("Successfully verified proof!");
+
+    // Save proof
+    let proof_path = "fibonacci_proof.bin";
+    proof.save(proof_path).expect("Failed to save proof");
+    println!("Proof saved to: {}", proof_path);
+
+    println!("\nSP1 ZK Proof generation complete!");
 }
